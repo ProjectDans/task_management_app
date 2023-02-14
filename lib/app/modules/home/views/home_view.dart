e1f974b65cf6bf7a0ef73ac0f026e9bf4718e00a
ProjectDans <dansproject0421@gmail.com>
2023-02-14 12:03:57 +0700

    update home view

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:ionicons/ionicons.dart';
+import 'package:task_management_app/app/routes/app_pages.dart';
+import 'package:task_management_app/app/utils/widget/sideBar.dart';
 
 import '../controllers/home_controller.dart';
 
@@ -12,25 +16,85 @@ class HomeView extends GetView<HomeController> {
         backgroundColor: Colors.blue[100],
         body: Row(
           children: [
-            Expanded(
+            const Expanded(
               flex: 2,
-              child: Container(
-                color: Colors.blue[100],
-                child: SingleChildScrollView(
-                    child: Column(
-                  children: [
-                    Container(
-                      height: 100,
-                      child: Image.asset('assets/icon.png'),
-                    )
-                  ],
-                )),
-              ),
+              child: SideBar(),
             ),
             Expanded(
               flex: 15,
-              child: Container(
-                color: Colors.white,
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: Get.height * 0.1,
+                    child: Padding(
+                      padding:
+                          const EdgeInsets.only(left: 40, right: 40, top: 25),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Task Management",
+                                style: GoogleFonts.poppins(
+                                    fontSize: 18, fontWeight: FontWeight.bold),
+                              ),
+                              Text(
+                                "Manage Task Easier With Friends",
+                                style: GoogleFonts.poppins(fontSize: 10),
+                              ),
+                            ],
+                          ),
+                          const Spacer(
+                            flex: 1,
+                          ),
+                          const Expanded(
+                            child: TextField(
+                              decoration: InputDecoration(
+                                filled: true,
+                                fillColor: Colors.white,
+                                contentPadding:
+                                    EdgeInsets.only(left: 20, right: 15),
+                                enabledBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(color: Colors.white),
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderSide:
+                                      BorderSide(color: Colors.blueAccent),
+                                ),
+                                prefixIcon: Icon(
+                                  Icons.search,
+                                  color: Colors.grey,
+                                ),
+                                hintText: "Search",
+                              ),
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 20,
+                          ),
+                          const Icon(
+                            Ionicons.notifications,
+                            color: Colors.grey,
+                          ),
+                          const SizedBox(
+                            width: 20,
+                          ),
+                          Text(
+                            "Sign Out",
+                            style: GoogleFonts.poppins(fontSize: 12),
+                          ),
+                          SizedBox(width: 5),
+                          Icon(
+                            Ionicons.log_out_outline,
+                            color: Colors.grey,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
           ],
---
+++ lib/app/utils/widget/sideBar.dart
@@ -0,0 +1,182 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:ionicons/ionicons.dart';
+import 'package:task_management_app/app/routes/app_pages.dart';
+
+class SideBar extends StatelessWidget {
+  const SideBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      elevation: 0,
+      child: Container(
+        padding: const EdgeInsets.only(top: 16),
+        height: Get.height,
+        color: Colors.blue[100],
+        child: SingleChildScrollView(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(
+              height: 100,
+              alignment: Alignment.topCenter,
+              child: Image.asset('assets/icon.png'),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            SizedBox(
+              height: 100,
+              child: Center(
+                child: InkWell(
+                  child: Column(
+                    children: [
+                      Container(
+                        height: 40,
+                        width: 100,
+                        decoration: Get.currentRoute == '/home'
+                            ? BoxDecoration(
+                                borderRadius: BorderRadius.circular(25),
+                                color: Colors.white,
+                              )
+                            : const BoxDecoration(),
+                        child: Icon(
+                          Get.currentRoute == '/home'
+                              ? Ionicons.desktop
+                              : Ionicons.desktop_outline,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 7,
+                      ),
+                      Text(
+                        "Home",
+                        style: GoogleFonts.poppins(
+                            fontSize: 14, color: Colors.grey),
+                      ),
+                    ],
+                  ),
+                  onTap: () => Get.toNamed(Routes.HOME),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 100,
+              child: Center(
+                child: InkWell(
+                  child: Column(
+                    children: [
+                      Container(
+                        height: 40,
+                        width: 100,
+                        decoration: Get.currentRoute == '/task'
+                            ? BoxDecoration(
+                                borderRadius: BorderRadius.circular(25),
+                                color: Colors.white,
+                              )
+                            : const BoxDecoration(),
+                        child: Icon(
+                          Get.currentRoute == '/task'
+                              ? Ionicons.cube
+                              : Ionicons.cube_outline,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 7,
+                      ),
+                      Text(
+                        "Task",
+                        style: GoogleFonts.poppins(
+                            fontSize: 14, color: Colors.grey),
+                      )
+                    ],
+                  ),
+                  onTap: () => Get.toNamed(Routes.TASK),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 100,
+              child: Center(
+                child: InkWell(
+                  child: Column(
+                    children: [
+                      Container(
+                        height: 40,
+                        width: 100,
+                        decoration: Get.currentRoute == '/friends'
+                            ? BoxDecoration(
+                                borderRadius: BorderRadius.circular(25),
+                                color: Colors.white,
+                              )
+                            : const BoxDecoration(),
+                        child: Icon(
+                          Get.currentRoute == '/friends'
+                              ? Ionicons.heart
+                              : Ionicons.heart_outline,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 7,
+                      ),
+                      Text(
+                        "Friends",
+                        style: GoogleFonts.poppins(
+                            fontSize: 14, color: Colors.grey),
+                      )
+                    ],
+                  ),
+                  onTap: () => Get.toNamed(Routes.FRIENDS),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 100,
+              child: Center(
+                child: InkWell(
+                  child: Column(
+                    children: [
+                      Container(
+                        height: 40,
+                        width: 100,
+                        decoration: Get.currentRoute == '/profile'
+                            ? BoxDecoration(
+                                borderRadius: BorderRadius.circular(25),
+                                color: Colors.white,
+                              )
+                            : const BoxDecoration(),
+                        child: Icon(
+                          Get.currentRoute == '/profile'
+                              ? Ionicons.person
+                              : Ionicons.person_outline,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 7,
+                      ),
+                      Text(
+                        "Profile",
+                        style: GoogleFonts.poppins(
+                            fontSize: 14, color: Colors.grey),
+                      )
+                    ],
+                  ),
+                  onTap: () => Get.toNamed(Routes.PROFILE),
+                ),
+              ),
+            ),
+          ],
+        )),
+      ),
+    );
+  }
+}
